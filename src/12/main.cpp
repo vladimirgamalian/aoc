1728ab754f243bc0835bba7e93afd871e2a87c6a
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2016-09-09 09:38:58 +0700

    Improve 12 code

--- src/12/main.cpp
+++ src/12/main.cpp
@@ -11,70 +11,31 @@ std::string readFile(const std::string& file_name) {
     return std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
 }
 
-int solve_part_a(const std::string& input) {
-    int sum = 0;
-    bool inside_string = false;
-    std::string number_string;
-    for (char c : input) {
-        if (c == '\"') {
-            inside_string = !inside_string;
-            continue;
-        }
-        if (inside_string)
-            continue;
-        if ((c >= '0' && c <= '9') || (c == '-')) {
-            number_string += c;
-        } else {
-            if (!number_string.empty()) {
-                int n = std::stoi(number_string);
-                number_string = "";
-                sum += n;
-            }
-        }
-    }
-    return sum;
-}
-
-int recursive_pass_obj(const json j);
-
-int recursive_pass_array(const json j) {
-    int sum = 0;
-    for (auto& e : j) {
-        if (e.is_array())
-            sum += recursive_pass_array(e);
-        else if (e.is_object())
-            sum += recursive_pass_obj(e);
-        else if (e.is_number())
-            sum += e.get<int>();
-    }
-    return sum;
-}
-
-int recursive_pass_obj(const json j) {
+int recursive_pass(const json& j, bool exclude_red) {
     int sum = 0;
     bool is_red = false;
-    for (json::const_iterator it = j.begin(); it != j.end(); ++it) {
-        json e = it.value();
-        if (e.is_array())
-            sum += recursive_pass_array(e);
-        else if (e.is_object())
-            sum += recursive_pass_obj(e);
-        else if (e.is_number())
-            sum += e.get<int>();
-        else if (e.is_string() && e.get<std::string>() == "red")
-            is_red = true;
+
+    if (j.is_array()) {
+        for (auto& e : j) {
+            sum += recursive_pass(e, exclude_red);
+        }
+    } else if (j.is_object()) {
+        for (json::const_iterator it = j.begin(); it != j.end(); ++it) {
+            json e = it.value();
+            sum += recursive_pass(e, exclude_red);
+            if (exclude_red && e.is_string() && e == "red")
+                is_red = true;
+        }
+    } else if (j.is_number()) {
+        sum = j;
     }
+
     return is_red ? 0 : sum;
 }
 
-int solve_part_b(const std::string& input) {
-    auto j = json::parse(input);
-    return j.is_array() ? recursive_pass_array(j) : recursive_pass_obj(j);
-}
-
 int main() {
-    std::string input = readFile("input.txt");
-    std::cout << solve_part_a(input) << "\n";
-    std::cout << solve_part_b(input) << "\n";
+    json j = json::parse(readFile("input.txt"));
+    std::cout << recursive_pass(j, false) << "\n";
+    std::cout << recursive_pass(j, true) << "\n";
     return 0;
 }
